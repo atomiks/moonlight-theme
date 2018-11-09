c9698b6bf5f843af2e3d5ef2cb1d056fcbeae629
atomiks <cc.glows@gmail.com>
2018-11-10 04:14:38 +1100

    Additional syntax examples

--- highlight.js
+++ highlight.js
@@ -1,15 +1,19 @@
-import Highlighter from "highlighter"
+import Highlighter from 'highlighter'
 
 /* Comment */
 class Moonlight extends Highlighter {
   constructor() {
     this.data = {
-      string: "hello",
+      string: 'hello',
       boolean: true && false,
       nil: undefined || null,
       regex: /^[12345](hello).+$/,
       number: -123456789,
-      jsx: <Component prop="moonlight" />,
+      jsx: (
+        <Component prop="moonlight">
+          <div />
+        </Component>
+      ),
       immutableConstant: ALL_CAPS,
       function: (...numbers) =>
         numbers.map(n => n + 2).filter(n => n <= numbers.length - 2)
